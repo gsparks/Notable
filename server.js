a8b4a2a3b326d2c13a0be815149915b5b644dbdd
Greg Sparks <gsparks@westmont.edu>
2019-05-02 21:53:43 -0700

    Added insert note

--- server.js
+++ server.js
@@ -7,10 +7,14 @@ const bodyParser     = require('body-parser');
 const app            = express();
 
 const port = 8000;
-
 app.use(bodyParser.urlencoded({ extended: true }));
-
-require('./app/routes')(app, {});
-app.listen(port, () => {
-  console.log('We are live on ' + port);
-});
+MongoClient.connect(db.url, (err, database) => {
+  if (err) return console.log(err)
+                      
+  // Make sure you add the database name and not the collection name
+  const database = database.db("Notable")
+  require('./app/routes')(app, database);
+  app.listen(port, () => {
+    console.log('We are live on ' + port);
+  });               
+})
